73d3d887d2a74b179791c3332d6f6dd49fa3d705
Shubham <105529358+Shubh99992@users.noreply.github.com>
2024-06-17 15:23:14 +0000

    Version-2 Initialize

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,10 +1,7 @@
-from django.contrib import admin
-from django.urls import include, path
-from app import views
+from django.urls import  path
 from app.views import HomeView
 urlpatterns = [
     path("", HomeView.as_view(), name="index"),
-    path("",views.index),
-    path("team/<int:s>/",views.cls)
+    
 ]
 
--- project/app/views.py
+++ project/app/views.py
@@ -1,14 +1,7 @@
-from django.shortcuts import render, HttpResponse
+
 from django.views.generic import TemplateView
 # Create your views here.
-def index(req):
-    return render(req, "index.html")
 
-def cls(r, s):
-    return HttpResponse(s)
-
-def custom_404(request, exception):
-    return render(request, '404.html', status=404)
     
 class HomeView(TemplateView):
     template_name = "review.html"
