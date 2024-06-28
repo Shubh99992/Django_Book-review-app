80bc43f1d01d9bd63332e9d08a3cb99752fb6c44
Prem Kumar <95973265+IMKnightRider@users.noreply.github.com>
2024-06-28 01:24:48 +0000

    added recent book and fav book style and script

--- app/static/script.js
+++ app/static/script.js
@@ -8,3 +8,21 @@ document.addEventListener("DOMContentLoaded", function() {
         content.classList.toggle("collapsed");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const collapsibleHeaders = document.querySelectorAll(".collapsible-header");
+
+    collapsibleHeaders.forEach(header => {
+        header.addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+                content.style.maxHeight = "0";
+            } else {
+                content.style.display = "block";
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    });
+});
