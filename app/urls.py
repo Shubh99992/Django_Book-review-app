519f9be2dbbbdf601fd425b35ea99da26182b9d6
Prem Kumar <95973265+IMKnightRider@users.noreply.github.com>
2024-06-28 07:13:51 +0000

    added urls from view

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from app.views import ExploreBooksView, SignUpView, HomeView, BookDetailView
+from app.views import CommunityView, ExploreBooksView, SignUpView, HomeView, BookDetailView, UserProfileView, follow_user, unfollow_user
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
@@ -7,6 +7,12 @@ urlpatterns = [
     path("book/<int:pk>/", BookDetailView.as_view(), name="book_details"),  # new
     path("accounts/", include("django.contrib.auth.urls")),
     path("explore/", ExploreBooksView.as_view(), name='explore_books'),
+    
+    path('profile/<str:username>/', UserProfileView.as_view(), name='user_profile'),
+    path('follow/<str:username>/', follow_user, name='follow_user'),
+    path('unfollow/<str:username>/', unfollow_user, name='unfollow_user'),
+    path('community/', CommunityView.as_view(), name='community'),
+    
     
     
 ]
